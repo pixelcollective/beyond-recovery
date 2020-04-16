fd66c2cb1d052170277ba8d9736ba47b13a868c4
Kelly Mears <developers@tinypixel.dev>
2020-04-15 19:20:11 -0700

    netlify uploadcare

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,5 +1,7 @@
 import CMS from 'netlify-cms-app'
+import uploadcare from 'netlify-cms-media-library-uploadcare'
 
 import ActionPagePreview from './preview-templates/ActionPagePreview'
 
+CMS.registerMediaLibrary(uploadcare)
 CMS.registerPreviewTemplate('action', ActionPagePreview)
