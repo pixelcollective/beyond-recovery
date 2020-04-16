8d49f942648de121554dc682c8ba1d7930749acc
Kelly Mears <developers@tinypixel.dev>
2020-04-16 12:39:25 -0700

    fix react-helmet at /donate

--- src/pages/donate.js
+++ src/pages/donate.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import Helmet from 'react-helmet'
+import { Helmet } from 'react-helmet'
 import Layout from '../components/layout'
 import { css } from '@emotion/core'
 import { Box } from 'rebass'
